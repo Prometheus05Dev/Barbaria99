92f8b4981caf8a422ac223a0c1b22bd2622b29ae
Prometheus05Dev <minecraftjanek05@gmail.com>
2020-04-07 21:00:09 +0200

    Implemented gamewindow. Hooray!

--- main.c
+++ main.c
@@ -13,11 +13,32 @@ bool shouldClose = false;
 
 int main() {
     printf("Hello, World!\n");
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow *mainWindow = glfwCreateWindow(1920, 1080, "Barbaria",glfwGetPrimaryMonitor(), NULL);
+    if(mainWindow == NULL) {
+        printf("Creation of game window failed!");
+        glfwTerminate();
+    }
+    glViewport(0, 0, 1920, 1080);
+    glfwMakeContextCurrent(mainWindow);
+    #ifdef _RUNTIME
+    glfwSetInputMode(mainWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    #endif
+
+    glewExperimental = GL_TRUE;
+    glewInit();
+
     getConfigPath();
     loadObjects();
     while(!shouldClose){
         glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(mainWindow);
+        glfwPollEvents();
     }
     freeObjects();
     return 0;
